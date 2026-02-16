acd8346bd96aa2171db16610720f8d5f72e3e6c6
brooklynprisbrey-duckie <brooklynprisbrey@mail.weber.edu>
2026-02-15 23:55:20 -0700

    Pointers. Bah. The ultimate array.

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,48 @@
 
 using namespace std;
 
+bool fillingVial(Vial& vial, int (*fillC)[4], char arr[]) {
+	for (int i; i <= 4; i++) {
+		int code = rand() % 4;
+		switch (code) {
+		case 0:
+			if ((*fillC)[0] < 4) {
+				vial.add(arr[0]);
+				(*fillC)[0] += 1;
+				break;
+			}
+		case 1:
+			if ((*fillC)[1] < 4) {
+				vial.add(arr[1]);
+				(*fillC)[1] += 1;
+				break;
+			}
+		case 2:
+			if ((*fillC)[2] < 4) {
+				vial.add(arr[2]);
+				(*fillC)[2] += 1;
+				break;
+			}
+		case 3:
+			if ((*fillC)[3] < 4) {
+				vial.add(arr[3]);
+				(*fillC)[3] += 1;
+				break;
+			}
+		}
+	}
+	return true;
+}
+
 int main() {
-	char arrVial[] = { 'a','a','b',0};
-	Vial vial1(arrVial, 3);
-	arrVial[0] = 'b';
-	arrVial[1] = 'b';
-	//arrVial[2] = 'b';
-	//arrVial[3] = 'a';
-	Vial vial2(arrVial, 3);
+	srand(time(0));
+	int fillCount[4] = {0};
+	char ultArr[4] = { 'b','d','p','q' };
 
-	vial1.display();
-	vial2.display();
+	Vial via1;
+	fillingVial(via1, &fillCount, ultArr);
 
-	vial1.transfer(vial2);
-
-	vial1.display();
-	vial2.display();
+	via1.display();
 
 	return 0;
 }
